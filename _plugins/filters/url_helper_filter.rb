cab6183b1727251a53b960d81de8deba575ed734
Danny Guinther <dannyguinther@gmail.com>
2015-08-08 08:52:38 -0400

    Replace byline tag with byline partial

--- _plugins/filters/url_helper_filter.rb
+++ _plugins/filters/url_helper_filter.rb
@@ -1,14 +1,20 @@
 require_relative "../util/archive_util"
+require_relative "../util/author_util"
 require_relative "../util/category_util"
 require_relative "../util/tag_util"
 
 module JekyllHueman
   module URLHelperFilter
-    include JekyllHueman::ArchiveUtil
-    include JekyllHueman::CategoryUtil
-    include JekyllHueman::TagUtil
+    include ArchiveUtil
+    include AuthorUtil
+    include CategoryUtil
+    include TagUtil
 
     def archive_url(*)
+      super
+    end
+
+    def author_url(*)
       super
     end
 
