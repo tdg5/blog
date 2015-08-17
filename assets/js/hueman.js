7eb8f4c0b297c2385cd53c832a4650ff7f66d2bc
Danny Guinther <dannyguinther@gmail.com>
2015-08-17 07:24:35 -0400

    Rename sidebar css classes to fix interaction with rouge styles

--- assets/js/hueman.js
+++ assets/js/hueman.js
@@ -1,17 +1,17 @@
 jQuery(document).ready(function($) {
   var body = $("body");
   function s1_toggle() {
-    body.toggleClass("s1-collapse").toggleClass("s1-expand");
+    body.toggleClass("sidebar-1-collapse").toggleClass("sidebar-1-expand");
   }
   function s2_toggle() {
-    body.toggleClass("s2-collapse").toggleClass("s2-expand");
+    body.toggleClass("sidebar-2-collapse").toggleClass("sidebar-2-expand");
   }
-  $(".s1 .sidebar-toggle").click(function() {
+  $(".sidebar-1 .sidebar-toggle").click(function() {
     s1_toggle();
-    if(body.is(".s2-expand")) { s2_toggle(); }
+    if(body.is(".sidebar-2-expand")) { s2_toggle(); }
   });
-  $(".s2 .sidebar-toggle").click(function() {
+  $(".sidebar-2 .sidebar-toggle").click(function() {
     s2_toggle();
-    if(body.is(".s1-expand")) { s1_toggle(); }
+    if(body.is(".sidebar-1-expand")) { s1_toggle(); }
   });
 });
